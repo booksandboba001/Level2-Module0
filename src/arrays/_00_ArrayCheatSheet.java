7edb88021c618602b8c7095d66831433a7021603
653955 <653955@SDUSD-MZ01PR8A.sdcs.local>
2026-01-14 16:50:08 -0800

    started new level

--- src/arrays/_00_ArrayCheatSheet.java
+++ src/arrays/_00_ArrayCheatSheet.java
@@ -1,7 +1,32 @@
 package arrays;
 
+import java.util.Random;
+
 public class _00_ArrayCheatSheet {
 	public static void main(String[] args) {
+		Random ran = new Random();
+		int rand = ran.nextInt(100);
+		String[] str = new String[6];
+		str[1] = "My favorite song at the moment is a three way tie between";
+		str[2]="Vanilla Twilight";
+		str[3]="All eyes on me";
+		str[4] = "and You'll be on my mind.";
+		str[5] = "I also like splatoon music, but we're not getting into that right now";
+		int smallest=100;
+		for(int i =1; i<6; i++) {
+			System.out.println(str[i]);
+		}
+		Integer[] toomany = new Integer[51];
+		for (int i = 50;i>0; i--) {
+			toomany[i] = rand;
+			rand = ran.nextInt(1000);
+			if(rand<smallest) {
+				smallest=rand;
+				toomany[i]=smallest;
+			}
+			System.out.println(toomany[i]);
+		}
+		System.out.println(smallest);
 		//1. make an array of 5 Strings
 		
 		//2. print the third element in the array
